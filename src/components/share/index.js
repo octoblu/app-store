880eaec0a8f6151d701662b0a1940a78e2817e57
Square Root of Saturn <sqrt@octoblu.com>
2016-01-29 15:12:05 -0700

    v1.0.7 update share url

--- src/components/share/index.js
+++ src/components/share/index.js
@@ -7,21 +7,20 @@ import EmailShareButton from './email-share-button'
 import FacebookShareButton from './facebook-share-button'
 import TwitterShareButton from './twitter-share-button'
 
-const Share = ({ className, importUrl }) => {
+const Share = ({ className }) => {
   const componentClass = classNames('Share', className)
 
   return <div className={componentClass}>
     <div className="Share-buttons">
-      <TwitterShareButton text={`Check out this awesome bluprint on @Octoblu ${importUrl}`} />
-      <FacebookShareButton url={importUrl} />
-      <EmailShareButton subject={`Octoblu is so cool" body="Check out this awesome bluprint from Octoblu ${importUrl}`} />
+      <TwitterShareButton text={`Check out this awesome bluprint on @Octoblu ${window.location.href}`} />
+      <FacebookShareButton url={window.location.href} />
+      <EmailShareButton subject={`Octoblu is so cool" body="Check out this awesome bluprint from Octoblu ${window.location.href}`} />
     </div>
   </div>
 }
 
 Share.propTypes = {
-  className: PropTypes.string,
-  importUrl: PropTypes.string.isRequired
+  className: PropTypes.string
 }
 
 export default Share
