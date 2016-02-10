0979de0b9e0779af8ebb75f50da455b7342e0c6f
sqrtofsaturn <sqrt@octoblu.com>
2016-02-10 15:33:36 -0700

    starting...

--- src/components/bluprint-card/index.js
+++ src/components/bluprint-card/index.js
@@ -8,22 +8,36 @@ import './index.css'
 const BluprintCard = ({ bluprint, collectionId }) => {
   const channelImages = _.map(bluprint.tags, (tag, index) => <ChannelImage key={index} name={tag} className="BluprintCard-channelImage" />)
 
-  return <div className={`BluprintCard BluprintCard--${bluprint.tags[0]}`}>
-    <header className="BluprintCard-header">
-      {channelImages}
-    </header>
+  // return <div className={`BluprintCard BluprintCard--${bluprint.tags[0]}`}>
+  //   <header className="BluprintCard-header">
+  //     {channelImages}
+  //   </header>
+  //
+  //   <footer className="BluprintCard-footer">
+  //     <p className="font-bold BluprintCard-label">{bluprint.label}</p>
+  //     <p className="BluprintCard-description">{bluprint.intro}</p>
+  //     <Link
+  //       to={`/collections/${collectionId}/bluprints/${bluprint.id}`}
+  //       className="Button Button--neutral BluprintCard-button"
+  //       key={bluprint.id}
+  //     >
+  //       Learn More
+  //     </Link>
+  //   </footer>
+  // </div>
 
-    <footer className="BluprintCard-footer">
-      <p className="font-bold BluprintCard-label">{bluprint.label}</p>
-      <p className="BluprintCard-description">{bluprint.intro}</p>
-      <Link
-        to={`/collections/${collectionId}/bluprints/${bluprint.id}`}
-        className="Button Button--neutral BluprintCard-button"
-        key={bluprint.id}
-      >
-        Learn More
-      </Link>
-    </footer>
+  return <div className={`BluprintCard BluprintCards--${bluprint.tags[0]}`}>
+    <h4 className="BluprintCard-label">{bluprint.label}</h4>
+
+    <p className="BluprintCard-description">{bluprint.intro}</p>
+
+    <Link
+      to={`/collections/${collectionId}/bluprints/${bluprint.id}`}
+      key={bluprint.id}
+    >
+      Learn More
+    </Link>
+
   </div>
 }
 
