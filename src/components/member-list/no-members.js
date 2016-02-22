a3e14592c7165e4d059ab20dafebaae0b2b4e4bc
sqrtofsaturn <sqrt@octoblu.com>
2016-02-22 12:45:51 -0700

    Delete unused components

--- src/components/member-list/no-members.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NoMembers = () => (
-  <div className="NoMembers">
-    <p>There are currently no members in your organization.</p>
-  </div>
-);
-
-export default NoMembers
