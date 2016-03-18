181366b9720dd7eb7c92a0bb1dd8cfcfd1dbecf8
ASteinheiser <asteinhe@asu.edu>
2016-03-18 16:47:41 -0700

    v2.2.21 plz travis

--- deploy/invalidate-cloudfront.sh
+++ deploy/invalidate-cloudfront.sh
@@ -6,8 +6,11 @@ install_awscli(){
 }
 
 configure_awscli(){
-  aws configure set aws_access_key_id "AKIAJH2D5IPIRHYJT5IA"
-  aws configure set aws_secret_access_key "c4AAOq1v934Bxlihi46UaXCahPvMLBRZs6QliWxBl0uU3JWnlKgmu7H6uW5BrGXvmNV5rjXe72byutSzbF/LaSuE4B6MgrqdGsDBNWd/gnYFf7zFhCB30FAu8qmQb1JPO8ulteLydzTSupJmqRwhykQjsRdiIQRJDETBcyxD8jnkopy6Di/UmWzMG2v4pGAlelBc0AfEM3AcuoTxUrcG/GEPT8IMHDV4286CVGndaV5qda3phpOSZybvNaF8uT/okut7IS/J68iXytLHbE1IVGGvfp4gT4eEi5EtwWbyeCOObCJU13atN3g+w+T65hQCWpPFVFNYh+ICYayN0RVSii5Tjy7vwBaqgUadJtVgxy2sqcx7UwRCuwAYsIbUZQvRQCuAkfN0mNLopf7sHzRjzuyROOlCO8zDM+C3carNBO3GN4Hce1c3kA7pIzcGDS02iu4fRXjxdJnXIoluJQFrICbke4LAPWx7rEDWOdXuYZcfGM9CR4EUcI5BfSpvmYjPuBm5zzEPK3jO5x4cbfZJQrD3rhr81e0qRspZHwk0GkI0c4U+1DZKVK/Oa220ISrfoOjyJXJHMFnRaqk7WNyZglubBLGoy6n6g1lB99HvyGmfC6ANhgJuu/40bYJcLqX5jo5BUQB37/6ONX32VK1ytOC2CnlKsWoujDpTRGGEEr0="
+  local aws_access_key_id=$1
+  local aws_secret_access_key=$2
+
+  aws configure set aws_access_key_id "$aws_access_key_id"
+  aws configure set aws_secret_access_key "$aws_secret_access_key"
   aws configure set preview.cloudfront true
 }
 
