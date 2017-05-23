59fb161f3517e23d7b36e997992e7bdb9c0e8b04
iamruinous / miql <jade.meskill@gmail.com , mxander@gmail.com>
2017-05-23 14:59:59 -0700

    v2.4.1 wait for authentication before rendering UI

--- src/containers/layout.js
+++ src/containers/layout.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router'
-import { connect } from 'react-redux'
+import React, { Component } from "react"
+import { Link } from "react-router"
+import { connect } from "react-redux"
 
-import '../styles/handglide/fonts.css'
-import '../styles/base.css'
+import "../styles/handglide/fonts.css"
+import "../styles/base.css"
 
-import AppBar from '../components/zooids/app-bar'
-import {OCTOBLU_URL} from '../constants'
+import AppBar from "../components/zooids/app-bar"
+import Loading from "../components/loading/index"
+import { OCTOBLU_URL } from "../constants"
 
 class Layout extends Component {
+  state = {
+    showApp: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener("message", (event) => {
+      if (event.origin === window.location.origin && event.data) {
+        if (event.data.name === "$cwcNavbarUserAuthorized") {
+          this.setState({ showApp: true })
+        }
+        if (event.data.name === '$cwcNavbarUserUnauthorized') {
+          this.setState({ showApp: false })
+        }
+      }
+    })
+  }
+
   render() {
-    const {context} = this.props
+    const { context } = this.props
+    const { showApp } = this.state
 
     let appBar = null
-    if (context === 'octoblu') appBar = <AppBar octobluUrl={ OCTOBLU_URL }/>
+    if (context === "octoblu") appBar = <AppBar octobluUrl={OCTOBLU_URL} />
 
-    return <div>
-      {appBar}
-      {this.props.children}
-    </div>
+    let children = <Loading message="Please wait while we authenticate you." />
+    if (showApp) children = this.props.children
+
+    return (
+      <div>
+        {appBar}
+        {children}
+      </div>
+    )
   }
 }
 
-function mapStateToProps({context}) {
-  return {context}
+function mapStateToProps({ context }) {
+  return { context }
 }
 
 export default connect(mapStateToProps)(Layout)
